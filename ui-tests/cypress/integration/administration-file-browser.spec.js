fb87e20092c75aeb351e33b4b06f0d29860a350d
Irakli Chalagashvili <chalagashvili5@gmail.com>
2021-06-30 23:46:24 +0400

    ENG-2242 add assertions to file and multiple file uploads

--- ui-tests/cypress/integration/administration-file-browser.spec.js
+++ ui-tests/cypress/integration/administration-file-browser.spec.js
@@ -15,7 +15,6 @@ describe('Pages', () => {
     cy.appBuilderLogout();
   });
 
-  // TODO add assertions to have real tests
   describe('Upload file in the administration', () => {
     it('Should upload one file', () => {
       cy.openPageFromMenu(['Administration', 'File browser']);
@@ -23,6 +22,7 @@ describe('Pages', () => {
       cy.getByTestId(TEST_ID_BUTTON_FILE_BROWSER).contains('Upload files').click();
       cy.addAttachFile(TEST_ID_UPLOAD_FIELD_FILE_BROWSER, 'upload/data1.json');
       cy.getByTestId(TEST_ID_ACTION_BUTTON_FILE_BROWSER).contains('Upload').click();
+      cy.getTableRowsBySelector('data1.json').should('be.visible');
     });
 
     it('Should upload multiple file', () => {
@@ -31,6 +31,8 @@ describe('Pages', () => {
       cy.getByTestId(TEST_ID_BUTTON_FILE_BROWSER).contains('Upload files').click();
       cy.addAttachFile(TEST_ID_UPLOAD_FIELD_FILE_BROWSER, ['upload/data2.json', 'upload/data3.json']);
       cy.getByTestId(TEST_ID_ACTION_BUTTON_FILE_BROWSER).contains('Upload').click();
+      cy.getTableRowsBySelector('data2.json').should('be.visible');
+      cy.getTableRowsBySelector('data3.json').should('be.visible');
     });
 
     it('Should upload file with drag and drop', () => {
