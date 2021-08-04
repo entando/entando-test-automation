adb1d1e5b74b41189120fdd3eaabf94e77945ed6
Rax Canaan Layumas <raxkaynan@gmail.com>
2021-08-04 17:25:45 +0800

    Move generateRandomContentTypeCode back to utils
    Avoids duplication since it's used in multiple files

--- ui-tests/cypress/support/utils.js
+++ ui-tests/cypress/support/utils.js
@@ -2,6 +2,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const generateRandomId = () => uuidv4().substr(0, 10).replace(/-/g, '_');
 
+export const generateRandomContentTypeCode = () => {
+  let code = "";
+  for (let i = 0; i < 3; i++) {
+    code += String.fromCharCode("A".charCodeAt(0) + Math.random() * 26);
+  }
+  return code;
+};
+
 // Page commands Utils
 export const replaceLangCodePlaceholder = (str, language) => str.replace('LANG-CODE', language);
 export const replaceMetaTagPlaceholders = (str, id, language) => str.replace('LANG-CODE', language).replace('META_TAG_ID', id);
