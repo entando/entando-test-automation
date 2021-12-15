1c97e380b5817eb47da67f89c85c84f3d5234b8b
g.dimartino <g.dimartino@entando.com>
2021-12-15 16:44:00 +0100

    Overwritten visit command to prepend a configurable basePath

--- ui-tests/cypress/support/command/command.js
+++ ui-tests/cypress/support/command/command.js
@@ -1,3 +1,7 @@
+Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
+  return originalFn(Cypress.config('basePath') + url, options);
+});
+
 Cypress.Commands.add('initWindowOpenChecker', () => {
   cy.window().then((win) => {
     cy.stub(win, 'open').as('windowOpen').callsFake(url => {
