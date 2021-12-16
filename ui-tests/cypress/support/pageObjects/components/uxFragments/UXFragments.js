17c99802db0e3c34cf3f517d292d4ea4bf721d5b
Jeff Go <j.go@entando.com>
2021-12-16 14:38:47 +0800

    ENG-3015 amend pagination check on ux fragment list page

--- ui-tests/cypress/support/pageObjects/components/uxFragments/UXFragments.js
+++ ui-tests/cypress/support/pageObjects/components/uxFragments/UXFragments.js
@@ -1,3 +1,38 @@
 import Content from '../../app/Content.js';
+import { DATA_TESTID, htmlElements } from '../../WebElement.js';
+import Pagination from '../../app/Pagination.js';
 
-export default class UXFragmentsPage extends Content {}
+export default class UXFragmentsPage extends Content {
+  searchForm       = `${htmlElements.form}.FragmentSearchForm[${DATA_TESTID}=list_FragmentSearchForm_form]`;
+  searchCodeInput  = `${htmlElements.input}#fragmentcode[name="code"]`;
+
+  getSearchForm() {
+    return this.get()
+        .find(this.searchForm);
+  }
+
+  getSearchCodeInput() {
+    return this.getSearchForm()
+        .find(this.searchCodeInput);
+  }
+
+  getSearchSubmitButton() {
+    return this.getSearchForm()
+        .find(`${htmlElements.button}[type=submit]`);
+  }
+
+  getTable() {
+    return this.get()
+               .find(htmlElements.table);
+  }
+
+  getTableRows() {
+    return this.getTable()
+               .children(htmlElements.tbody)
+               .children(htmlElements.tr);
+  }
+
+  getPagination() {
+    return new Pagination(this);
+  }
+}
