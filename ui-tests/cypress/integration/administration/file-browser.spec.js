5e97c014ecda8aacf6c2c2950ac430861224fd5c
Guilherme <guilherme.r89@gmail.com>
2022-01-04 12:06:41 -0300

    adding file-browser scenario

--- ui-tests/cypress/integration/administration/file-browser.spec.js
+++ ui-tests/cypress/integration/administration/file-browser.spec.js
@@ -51,6 +51,30 @@ describe('File browser', () => {
       });
     });
 
+    it('Delete file with parenthesis in the filename', () => {
+      const filename = 'data(with-parenthesis).json';
+      currentPage.getContent().openSubFolder(0);
+
+      currentPage = currentPage.getContent().openUploadFilesPage();
+      currentPage.getContent().attachUploadFilesInput(`upload/${filename}`);
+      currentPage = currentPage.getContent().confirmUpload();
+      // deletion will be performed through screen
+      cy.wrap([]).as('fileToBeDeleted');
+
+      cy.validateToast(currentPage);
+      cy.closeAllToasts(currentPage);
+      currentPage.getContent().getTableRows().then(rows => {
+        cy.wrap(rows).contains(filename).should('be.visible');
+        // delete file
+        cy.wrap(rows).contains(filename).parents(htmlElements.tr).children(htmlElements.td).eq(-1).find(htmlElements.button).eq(0).click();
+        cy.wrap(rows).contains(filename).parents(htmlElements.tr).children(htmlElements.td).eq(-1).find(htmlElements.a).eq(1).click();
+        cy.wait(1000); // wait modal to open
+        cy.get(`${htmlElements.button}[id=DeleteFileModal__button-delete]`).click();
+        cy.wait(1000); // wait toast to show up
+        cy.validateToast(currentPage);
+      });
+    });
+
     xit('Upload a json file via drag n drop', () => {
       //TODO find how to simulate file drag 'n' drop
     });
