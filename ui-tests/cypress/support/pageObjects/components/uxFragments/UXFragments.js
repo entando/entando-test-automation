03d8d05675f4422b982fc4d9ef250bb3ceee4d65
Rax Canaan Layumas <raxkaynan@gmail.com>
2022-01-06 03:05:17 +0800

    Add ux fragments tests

--- ui-tests/cypress/support/pageObjects/components/uxFragments/UXFragments.js
+++ ui-tests/cypress/support/pageObjects/components/uxFragments/UXFragments.js
@@ -1,10 +1,16 @@
 import Content from '../../app/Content.js';
-import { DATA_TESTID, htmlElements } from '../../WebElement.js';
+import { DATA_ID, DATA_TESTID, htmlElements } from '../../WebElement.js';
 import Pagination from '../../app/Pagination.js';
+import AppPage from '../../app/AppPage.js';
+import AddPage from './AddPage';
+import KebabMenu from '../../app/KebabMenu.js';
+import DeleteDialog from '../../app/DeleteDialog.js';
 
 export default class UXFragmentsPage extends Content {
+
   searchForm       = `${htmlElements.form}.FragmentSearchForm[${DATA_TESTID}=list_FragmentSearchForm_form]`;
   searchCodeInput  = `${htmlElements.input}#fragmentcode[name="code"]`;
+  addBtn           = `${htmlElements.button}[${DATA_TESTID}=list_FragmentListContent_Button]`;
 
   getSearchForm() {
     return this.get()
@@ -35,4 +41,51 @@ export default class UXFragmentsPage extends Content {
   getPagination() {
     return new Pagination(this);
   }
+
+  getAddButton() {
+    return this.get()
+               .find(this.addBtn);
+  }
+
+  getKebabMenu(code) {
+    return new FragmentsKebabMenu(this, code);
+  }
+
+  openAddFragmentPage() {
+    this.getAddButton().click();
+    cy.wait(1000); // TODO find a better way to identify when the page loaded
+    return new AppPage(AddPage);
+  }
+}
+
+class FragmentsKebabMenu extends KebabMenu {
+
+  get() {
+    return this.parent.getTableRows()
+               .find(`#${this.code}-actions`)
+               .closest(htmlElements.div);
+  }
+
+  getEdit() {
+    return this.get()
+               .find(`[${DATA_ID}=edit-${this.code}]`)
+               .eq(0);
+  }
+
+  getDelete() {
+    return this.get()
+               .find(`.FragmentListMenuAction__menu-item-delete`);
+  }
+
+  openEdit() {
+    this.getEdit().click();
+    cy.wait(1000); // TODO find a better way to identify when the page loaded
+    return new AppPage(AddPage);
+  }
+
+  clickDelete() {
+    this.getDelete().click();
+    this.parent.parent.getDialog().setBody(DeleteDialog);
+  }
+
 }
