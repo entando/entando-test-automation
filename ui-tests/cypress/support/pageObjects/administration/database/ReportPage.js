4e1a18c1e3c6b6133c24a5c7e60a7eadaca91c26
Riccardo Fadda <r.fadda@entando.com>
2022-03-01 10:56:51 +0100

    ENG-3239 datasource details

--- ui-tests/cypress/support/pageObjects/administration/database/ReportPage.js
+++ ui-tests/cypress/support/pageObjects/administration/database/ReportPage.js
@@ -5,6 +5,8 @@ export default class ReportPage extends Content {
 
   descriptionList = `${htmlElements.dl}.dl-horizontal`
   componentTable = `${htmlElements.table}.ReportDatabaseListTable__table`
+  dataSourceTables = `${htmlElements.table}.ReportDatabaseDataSource__table`;
+  dataSourceCollapse = `${htmlElements.a}[role=button]`;
 
   getDescriptionList() {
     return this.getContents().find(this.descriptionList);
@@ -16,6 +18,28 @@ export default class ReportPage extends Content {
 
   getComponentTable() {
     return this.getContents().find(this.componentTable);
+  }
+
+  getDataSourceTables() {
+    return this.getContents().find(this.dataSourceTables);
+  }
+
+  openDataSource() {
+    this.getContents().find(this.dataSourceCollapse).click();
+  }
+
+  getDataSourcePortTableRowByIndex(index) {
+    return this.getDataSourceTables().eq(0)
+      .children(htmlElements.tbody)
+      .children(htmlElements.tr).eq(index)
+      .children(htmlElements.td);
+  }
+
+  getDataSourceServTableRowByIndex(index) {
+    return this.getDataSourceTables().eq(1)
+      .children(htmlElements.tbody)
+      .children(htmlElements.tr).eq(index)
+      .children(htmlElements.td);
   }
 
 }
