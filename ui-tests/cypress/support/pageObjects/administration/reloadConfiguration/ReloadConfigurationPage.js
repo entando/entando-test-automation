01944c3ca46a845f331e852f6b43d5a3e179fc97
Riccardo Fadda <r.fadda@entando.com>
2022-03-02 10:59:39 +0100

    ENG-3296 successful message displayed when clicking reload button

--- ui-tests/cypress/support/pageObjects/administration/reloadConfiguration/ReloadConfigurationPage.js
+++ ui-tests/cypress/support/pageObjects/administration/reloadConfiguration/ReloadConfigurationPage.js
@@ -4,9 +4,18 @@ import {htmlElements} from '../../WebElement.js';
 export default class ReloadConfigurationPage extends Content {
 
   reloadConfigurationButton = `${htmlElements.button}[type=button].ReloadConfig__reload-button`;
+  reloadConfirmDiv = `${htmlElements.div}.ReloadConfirm`;
 
   getReloadButton(){
     return this.getContents().find(this.reloadConfigurationButton);
+  }
+
+  clickReloadButton(){
+    this.getReloadButton().click();
+  }
+
+  getReloadConfirmation(){
+    return this.getContents().find(this.reloadConfirmDiv);
   }
 
 }
