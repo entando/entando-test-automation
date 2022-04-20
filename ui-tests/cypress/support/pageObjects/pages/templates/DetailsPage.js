e5aad41be50bba53f46a7c6d32a90340462c0152
Riccardo Fadda <r.fadda@entando.com>
2022-04-20 16:55:31 +0200

    ENG-3525 When navigating out of details page the template is not changed

--- ui-tests/cypress/support/pageObjects/pages/templates/DetailsPage.js
+++ ui-tests/cypress/support/pageObjects/pages/templates/DetailsPage.js
@@ -1,4 +1,6 @@
+import AppPage        from "../../app/AppPage";
 import Content        from "../../app/Content";
+import TemplatesPage  from "./TemplatesPage";
 import {htmlElements} from "../../WebElement";
 
 export default class DetailsPage extends Content {
@@ -28,6 +30,11 @@ export default class DetailsPage extends Content {
 
   getEditButton() {
     return this.getContents().find(this.editButton);
+  }
+
+  openTemplatesUsingBreadCrumb() {
+    this.getBreadCrumb().children(htmlElements.li).eq(1).then(element => TemplatesPage.openPage(element));
+    return cy.wrap(new AppPage(TemplatesPage));
   }
 
 }
