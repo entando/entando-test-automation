80d18f0162787c141e7a31141a2ba76dcfc89678
g.dimartino <g.dimartino@entando.com>
2022-04-22 12:12:27 +0200

    Updated reload configuration tests to intercept the page load GET API call Updated reload configuration tests to use an alias for currentPage

--- ui-tests/cypress/support/pageObjects/administration/reloadConfiguration/ReloadConfigurationPage.js
+++ ui-tests/cypress/support/pageObjects/administration/reloadConfiguration/ReloadConfigurationPage.js
@@ -1,21 +1,27 @@
-import AppContent     from '../../app/AppContent';
 import {htmlElements} from '../../WebElement.js';
+
+import AppContent from '../../app/AppContent';
+
+import AppPage from '../../app/AppPage';
 
 export default class ReloadConfigurationPage extends AppContent {
 
-  reloadConfigurationButton = `${htmlElements.button}[type=button].ReloadConfig__reload-button`;
-  reloadConfirmDiv          = `${htmlElements.div}.ReloadConfirm`;
-
   getReloadButton() {
-    return this.getContents().find(this.reloadConfigurationButton);
-  }
-
-  clickReloadButton() {
-    this.getReloadButton().click();
+    return this.getContents().find(`${htmlElements.button}[type=button].ReloadConfig__reload-button`);
   }
 
   getReloadConfirmation() {
-    return this.getContents().find(this.reloadConfirmDiv);
+    return this.getContents().find(`${htmlElements.div}.ReloadConfirm`);
+  }
+
+  reload() {
+    this.getReloadButton()
+        .then(button => {
+          cy.intercept('POST', 'http://test-7-0-0-final-cypress.apps.ent64azure.com/entando-de-app/api/reloadConfiguration').as('reloadConfigurationPOST');
+          cy.get(button).click();
+          cy.wait('@reloadConfigurationPOST');
+        });
+    return cy.wrap(new AppPage(ReloadConfigurationPage)).as('currentPage');
   }
 
 }
