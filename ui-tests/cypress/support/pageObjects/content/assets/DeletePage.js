47c976d1de8cc2ba09cc181a900a56e373b35b4f
GianMartinoEntando <g.martino@entando.com>
2022-05-09 17:01:20 +0200

    assets pageObjects fixed

---
+++ ui-tests/cypress/support/pageObjects/content/assets/DeletePage.js
@@ -0,0 +1,24 @@
+import {htmlElements}  from '../../WebElement.js';
+
+import AdminContent    from '../../app/AdminContent';
+
+
+export default class DeletePage extends AdminContent {
+
+  static openPage(button){
+    cy.assetsAdminConsoleController().then(controller => controller.intercept({method:'GET'}, 'deletePageLoadingGet', '/trash.action?*'));
+    cy.get(button).click();
+    cy.wait('@deletePageLoadingGet');
+  }
+
+  getAlert(){
+    return this.get()
+        .find(`${htmlElements.div}.alert`);
+  }
+  getAlertText(){
+    return this.getAlert()
+        .children(htmlElements.p)
+  }
+
+
+}
