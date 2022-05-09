9f38c38231c362a0afdd5a0c7a8078cd2f02cedc
g.dimartino <g.dimartino@entando.com>
2022-05-09 12:38:01 +0200

    Created getArrayRandomElement in utils.js

--- ui-tests/cypress/support/utils.js
+++ ui-tests/cypress/support/utils.js
@@ -5,6 +5,10 @@ import RandExp        from 'randexp';
 // const emailRegex = new RandExp(/(?=^[^@]{2,64}@[^@]{5,255}$)^([a-z][a-z0-9]*([-._][a-z0-9]+)*)@(([a-z][-a-z0-9]+([-.][a-z0-9]+)*\.[a-z0-9]{2,4})|(((25[0-5])|(2[0-4][0-9])|(1?[1-9]?[0-9])\.){3}((25[0-5])|(2[0-4][0-9])|(1?[1-9]?[0-9]))))$/);
 const emailRegex = new RandExp(/^([a-z][a-z0-9]{1,9}([-._][a-z0-9]{1,9}){0,5})@(([a-z][-a-z0-9]{4}([-.][a-z0-9]{1,9}){0,10}\.[a-z0-9]{2,4})|((((25[0-5])|(2[0-4][0-9])|(1?[1-9]?[0-9]))\.){3}((25[0-5])|(2[0-4][0-9])|(1?[1-9]?[0-9]))))$/);
 
+export const getArrayRandomElement = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 export const generateRandomId = () => uuidv4().substr(0, 10).replace(/-/g, '_');
 
 export const generateRandomTypeCode = () => {
