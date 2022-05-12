8570b91ab86ef64b296bbcba5f056c04e72ff48f
GianMartinoEntando <g.martino@entando.com>
2022-05-12 12:24:15 +0200

    assets.spec.js Refactoring of test cases and update of methods

--- ui-tests/cypress/integration/content/assets.spec.js
+++ ui-tests/cypress/integration/content/assets.spec.js
@@ -1,9 +1,6 @@
-import HomePage       from '../../support/pageObjects/HomePage';
 import {htmlElements} from '../../support/pageObjects/WebElement';
 
 describe('Assets', () => {
-
-  let currentPage;
 
   beforeEach(() => {
     cy.wrap(null).as('assetToBeDeleted');
@@ -23,19 +20,32 @@ describe('Assets', () => {
 
   describe([Tag.GTS], 'Add asset', () => {
 
+
     it('Add asset', () => {
-      cy.assetsController().then(controller => controller.intercept({method: 'POST'}, 'interceptedPOST'));
 
-      currentPage = openAssetsPage();
-      currentPage.getContent().selectFiles('cypress/fixtures/upload/image1.JPG');
-      currentPage.getDialog().getBody().getGroupSelect().select('administrators');
-      currentPage.getDialog().getBody().submit();
+      openAssetsPage()
 
-      cy.wait('@interceptedPOST').then(interception => cy.wrap(interception.response.body.payload).as('assetToBeDeleted'));
-
-      currentPage.getContent().getTableRows().then(rows =>
-          cy.wrap(rows).eq(0).children(htmlElements.td).eq(2).should('contain.text', 'image1.JPG')
-      );
+          .then(page => page.getContent().openAddAssets())
+          .then(page => {
+            page.getContent().selectFiles('cypress/fixtures/upload/image1.JPG');
+            page.getContent().getGroupSelect().select('administrators');
+            page.getContent().submit();
+          })
+          .then(page => {
+            page.getContent().getTableRows().then(rows =>
+                cy.wrap(rows).children(htmlElements.div).should('contain.text', 'image1.JPG')
+            );
+          })
+          .then(() => {
+            cy.assetsController()
+              .then((controller) => {
+                controller.getAssetsList()
+                          .then((response) => {
+                            const {body: {payload}} = response;
+                            cy.wrap(payload[0]).as('assetToBeDeleted');
+                          });
+              });
+          });
     });
 
   });
@@ -51,21 +61,24 @@ describe('Assets', () => {
     describe([Tag.GTS], 'Delete asset', () => {
 
       it('Delete asset', () => {
-        currentPage = openAssetsPage();
-        cy.wait(2000);
+        openAssetsPage()
+            .then(page => {
+              cy.get('@assetToBeDeleted')
+                .then(assetToBeDeleted => page.getContent().getKebabMenu(assetToBeDeleted.id).openDropdown().clickDelete())
+                .then(page => page.getContent().submit())
+                .then(page => {
 
-        cy.get('@assetToBeDeleted')
-          .then(assetToBeDeleted => currentPage.getContent().getKebabMenu(assetToBeDeleted.id).open().clickDelete());
-        cy.wait(1000);
-        currentPage.getDialog().confirm();
-
-        cy.validateToast(currentPage);
-
-        cy.wrap(null).as('assetToBeDeleted');
+                  page.getContent().getTableRows().then(rows =>
+                      cy.wrap(rows).children(htmlElements.div).should('not.contain.text', 'image1.JPG')
+                  );
+                });
+              cy.wrap(null).as('assetToBeDeleted');
+            });
       });
 
       it('Delete asset referenced by a content - not allowed', () => {
         let content;
+        let isForbidden = true;
 
         cy.get('@assetToBeDeleted').then(assetToBeDeleted =>
             content = {
@@ -97,23 +110,19 @@ describe('Assets', () => {
           .then(response => content.id = response.body.payload[0].id);
         cy.contentsController().then(controller => controller.updateStatus(content.id, 'published'));
 
-        currentPage = openAssetsPage();
-        //TODO validate this wait
-        cy.wait(2000);
+        openAssetsPage().then(page => {
+          cy.get('@assetToBeDeleted')
+            .then(assetToBeDeleted =>
+                page.getContent().getKebabMenu(assetToBeDeleted.id).openDropdown().clickDelete(isForbidden)
+            )
+            .then(page =>
+                page.getContent().getAlertText().should('have.text', 'Sorry. You cannot perform this action right now, you must first unlink the following cross-references.')
+            );
 
-        cy.get('@assetToBeDeleted').then(assetToBeDeleted =>
-            currentPage.getContent().getKebabMenu(assetToBeDeleted.id).open().clickDelete()
-        );
-        cy.wait(1000);
-        currentPage.getDialog().confirm();
-
-        cy.get('@assetToBeDeleted').then(assetToBeDeleted =>
-            cy.validateToast(currentPage, assetToBeDeleted.id, false)
-        );
-
-        cy.contentsController().then(controller => {
-          controller.updateStatus(content.id, 'draft');
-          controller.deleteContent(content.id);
+          cy.contentsController().then(controller => {
+            controller.updateStatus(content.id, 'draft');
+            controller.deleteContent(content.id);
+          });
         });
       });
 
@@ -122,69 +131,87 @@ describe('Assets', () => {
     describe([Tag.GTS], 'Edit asset', () => {
 
       it('Edit description', () => {
-        currentPage = openAssetsPage();
-        //TODO validate this wait
-        cy.wait(2000);
-        cy.get('@assetToBeDeleted')
-          .then(assetToBeDeleted => currentPage.getContent().getKebabMenu(assetToBeDeleted.id).open().openEdit())
-          .then(() => {
-            currentPage.getDialog().getBody().getDescriptionInput().type('test');
-            currentPage.getDialog().confirm();
-          });
-
-        cy.validateToast(currentPage, 'test');
+        openAssetsPage()
+            .then(page => {
+              cy.get('@assetToBeDeleted')
+                .then(assetToBeDeleted => {
+                  page.getContent().getKebabMenu(assetToBeDeleted.id).openDropdown().openEdit();
+                })
+                .then(page => {
+                  page.getContent().getDescriptionInput().clear().type('test');
+                  page.getContent().submit();
+                })
+                .then(page => {
+                  page.getContent().getTableRows().then(rows =>
+                      cy.wrap(rows).children(htmlElements.div).should('contain.text', 'test'));
+                });
+            });
       });
 
       it('Crop image', () => {
-        currentPage = openAssetsPage();
-        cy.wait(2000);
-        cy.get('@assetToBeDeleted')
-          .then(assetToBeDeleted => currentPage.getContent().getKebabMenu(assetToBeDeleted.id).open().openEdit())
-          .then(() => {
-            currentPage.getDialog().getBody().crop(-100, -100);
-            currentPage.getDialog().confirm();
-          });
-
-        cy.validateToast(currentPage);
+        openAssetsPage()
+            .then(page => {
+              cy.get('@assetToBeDeleted')
+                .then(assetToBeDeleted =>
+                    page.getContent().getKebabMenu(assetToBeDeleted.id).openDropdown().openEdit())
+                .then(page => {
+                  page.getContent().openDropDown().openEdit();
+                })
+                .then(page => {
+                  page.getAdminDialog().getBody().crop(-100, -100);
+                  page.getAdminDialog().close();
+                  page.getContent().submit();
+                })
+                page.getContent().getTableRows().then(rows =>
+                    cy.wrap(rows).children(htmlElements.div).should('contain.text', 'image1.JPG')
+                );
+            });
       });
 
       it('Rotate image', () => {
-        currentPage = openAssetsPage();
-        cy.wait(2000);
-        cy.get('@assetToBeDeleted')
-          .then(assetToBeDeleted => currentPage.getContent().getKebabMenu(assetToBeDeleted.id).open().openEdit())
-          .then(() => {
-            currentPage.getDialog().getBody().rotate('left');
-            currentPage.getDialog().confirm();
-          });
+        openAssetsPage()
+            .then(page => {
+              cy.get('@assetToBeDeleted')
+                .then(assetToBeDeleted => page.getContent().getKebabMenu(assetToBeDeleted.id).openDropdown().openEdit())
+                .then(page => {
+                  page.getContent().openDropDown().openEdit();
+                })
+                .then(page => {
+                  page.getAdminDialog().getBody().rotate();
+                  page.getAdminDialog().close();
+                  page.getContent().submit();
+                })
+              page.getContent().getTableRows().then(rows =>
+                  cy.wrap(rows).children(htmlElements.div).should('contain.text', 'image1.JPG')
+              );
+            });
 
-        cy.validateToast(currentPage);
+      });
+
+    });
+
+    describe(['ENG-2680'], 'Asset Browsing', () => {
+
+      it('Displaying correct item counts when searching with zero results', () => {
+        openAssetsPage()
+            .then(page => {
+              page.getContent().openAdvancedFilter();
+              page.getContent().getSearchTextfield().clear();
+              page.getContent().getSearchTextfield().type('z');
+              page.getContent().submitSearch();
+              page.getContent().getFilterResultItemCount().invoke('text').should('be.equal', 'Your search returned 0 results |\n' +
+                  '    Page 0 - 0');
+            });
       });
 
     });
 
   });
-
-  describe(['ENG-2680'], 'Asset Browsing', () => {
-
-    it('Displaying correct item counts when searching with zero results', () => {
-      currentPage = openAssetsPage();
-      currentPage.getContent().getSearchTextfield().clear();
-      currentPage.getContent().getSearchTextfield().type('z');
-      currentPage.getContent().getSearchButton().click();
-      cy.wait(1000);
-      currentPage.getContent().getFilterResultItemCount().invoke('text').should('be.equal', '0 of 0 items');
-    });
-
-  });
-
   const testFileInfo = {path: 'upload/image1.JPG', name: 'image1.JPG', type: 'image/jpeg'};
   const testMetadata = {group: 'administrators', categories: [], type: 'image'};
 
   const openAssetsPage = () => {
-    currentPage = new HomePage();
-    currentPage = currentPage.getMenu().getContent().open();
-    return currentPage.openAssets();
+    return cy.get('@currentPage')
+             .then(page => page.getMenu().getContent().open().openAssets());
   };
-
 });
