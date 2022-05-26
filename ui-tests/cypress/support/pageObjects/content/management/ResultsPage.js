77fca57ef2e389a2a4f2eb3d25221a6633ad39b0
GianMartinoEntando <g.martino@entando.com>
2022-05-26 10:48:22 +0200

    openViewResultsPage intercept defined

--- ui-tests/cypress/support/pageObjects/content/management/ResultsPage.js
+++ ui-tests/cypress/support/pageObjects/content/management/ResultsPage.js
@@ -1,15 +1,21 @@
 import AdminContent from '../../app/AdminContent';
+import AdminPage    from '../../app/AdminPage';
 
 export default class ResultsPage extends AdminContent {
 
-  static openDeleteContentsPage(button) {
+  //FIXME AdminConsole is not built on REST APIs
+  static openConfirmActionPage(button) {
+    cy.contentsAdminConsoleController().then(controller => controller.intercept({method: 'POST'}, 'viewActionPOST', '/search.action'));
     cy.get(button).click();
+    cy.wait('@viewActionPOST');
   }
 
-  static openUnPublishContentsPage(button) {
+  //FIXME AdminConsole is not built on REST APIs
+  static openViewResultsPage(button) {
+    cy.contentsAdminConsoleController().then(controller => controller.intercept({method: 'POST'}, 'resultsLoadingPOST', '/Bulk/*'));
     cy.get(button).click();
+    cy.wait('@resultsLoadingPOST');
   }
-
 
   getActionOnContent() {
     return this.get()
@@ -17,8 +23,8 @@ export default class ResultsPage extends AdminContent {
   }
 
   submit() {
-    this.getActionOnContent().then(button => this.click(button));
-    return cy.get('@currentPage');
+    this.getActionOnContent().then(button => ResultsPage.openViewResultsPage(button));
+    return cy.wrap(new AdminPage(ResultsPage)).as('currentPage');
   }
 
   getStatus() {
