6d8fb5d62628a29e642b4e8d2e899e44d3bf2952
GianMartinoEntando <g.martino@entando.com>
2022-05-26 10:53:29 +0200

    management.spec.js reformatted

--- ui-tests/cypress/integration/content/management.spec.js
+++ ui-tests/cypress/integration/content/management.spec.js
@@ -51,7 +51,7 @@ describe([Tag.GTS], 'Contents', () => {
     });
 
     // The save button is always enabled, the validation seems to happen on backend side; when trying to save without group, the default one is selected
-    //it('Add content without an owner group - not allowed', () => {});
+    // it('Add content without an owner group - not allowed', () => {});
 
     // Saving a content with only default language is allowed
     // it('Add new content and does not fill values for any language but the default one, a modal must present to inform for other languages (ENG-2714)', () => {});
@@ -65,7 +65,7 @@ describe([Tag.GTS], 'Contents', () => {
       openContentMgmtPage()
           .then(page => {
             cy.get('@contentToBeDeleted').then(content => {
-              page.getContent().getKebabMenu(content).openDropdown().openEdit()
+              page.getContent().getKebabMenu(content).open().openEdit()
                   .then(page => page.getContent().getContentDescriptionInput().then(input => {
                     page.getContent().clear(input);
                     page.getContent().type(input, updatedDescription);
@@ -78,7 +78,7 @@ describe([Tag.GTS], 'Contents', () => {
 
     it('Update status of content referenced by a published page', () => {
 
-      addTestPage();
+      addPublishedPage();
       openContentMgmtPage()
           .then(page =>
               cy.get('@contentToBeDeleted').then(content => {
@@ -86,10 +86,10 @@ describe([Tag.GTS], 'Contents', () => {
                 page.getContent().clickUnPublish();
               }))
           .then(page => page.getContent().submit())
-          .then(page => page.getContent().getAlertDanger().should('exist').and('be.visible'))
+          .then(page => page.getContent().getAlertDanger().should('exist').and('be.visible'));
       cy.wrap(null).as('contentToBeDeleted');
-      removeTestPage();
-    })
+      removePublishedPage();
+    });
   });
 
   describe('Delete content', () => {
@@ -100,136 +100,134 @@ describe([Tag.GTS], 'Contents', () => {
           .then(page => {
             cy.get('@contentToBeDeleted').then(content => {
                 page.getContent().getContentCheckBox(content).check({force: true});
-                page.getContent().clickDelete()
+                page.getContent().clickDelete();
               })
               .then(page => page.getContent().submit())
-              .then(page => page.getContent().getStatus().should('exist').and('be.visible'))
+              .then(page => page.getContent().getStatus().should('exist').and('be.visible'));
             cy.wrap(null).as('contentToBeDeleted');
           });
     });
 
-      it('Delete published content - not allowed', () => {
+    it('Delete published content - not allowed', () => {
 
-        addPublishedContent();
-        openContentMgmtPage()
-            .then(page => {
-              cy.get('@contentToBeDeleted').then(content => {
-                  page.getContent().getContentCheckBox(content).check({force: true});
-                  page.getContent().clickDelete()
-                })
-                .then(page => page.getContent().submit())
-                .then(page => page.getContent().getAlertDanger().should('exist').and('be.visible'));
-              cy.wrap(null).as('contentToBeDeleted');
-            });
-        removePublishedContent();
-      })
-
-
+      addPublishedContent();
+      openContentMgmtPage()
+          .then(page => {
+            cy.get('@contentToBeDeleted').then(content => {
+                page.getContent().getContentCheckBox(content).check({force: true});
+                page.getContent().clickDelete();
+              })
+              .then(page => page.getContent().submit())
+              .then(page => page.getContent().getAlertDanger().should('exist').and('be.visible'));
+            cy.wrap(null).as('contentToBeDeleted');
+          });
+      removePublishedContent();
+    });
   });
 
-    const DEFAULT_GROUP        = 'Free Access';
-    const DEFAULT_CONTENT_TYPE = 'Banner';
-    const testContent          = {
-      typeCode: 'BNR',
-      description: generateRandomId(),
-      mainGroup: 'Free Access',
-      attributes: [
-        {
-          code: 'title',
-          values: {
-            en: generateRandomId(),
-            it: generateRandomId()
-          }
+  const DEFAULT_GROUP        = 'Free Access';
+  const DEFAULT_CONTENT_TYPE = 'Banner';
+  const testContent          = {
+    typeCode: 'BNR',
+    description: generateRandomId(),
+    mainGroup: 'Free Access',
+    attributes: [
+      {
+        code: 'title',
+        values: {
+          en: generateRandomId(),
+          it: generateRandomId()
         }
-      ]
-    };
-    const contentTypeCode      = generateRandomTypeCode();
-    const contentTypeName      = generateRandomId();
-
-    const page = {
-      charset: 'utf-8',
-      code: 'test',
-      contentType: 'text/html',
-      pageModel: '1-2-column',
-      parentCode: 'homepage',
-      titles: {en: 'Test'},
-      ownerGroup: 'administrators'
-    };
-
-    const content = {
-      description: 'test',
-      mainGroup: 'administrators'
-    };
-
-    let contentId = {value: null};
-
-    const pageWidget = {
-      frameId: 4,
-      code: 'content_viewer',
-      config: {
-        ownerGroup: page.ownerGroup,
-        joinGroups: [],
-        contentDescription: content.description
       }
-    };
+    ]
+  };
+  const contentTypeCode      = generateRandomTypeCode();
+  const contentTypeName      = generateRandomId();
 
-    const updatedDescription  = `${generateRandomId()}`;
-    const openContentMgmtPage = () => cy.get('@currentPage').then(page => page.getMenu().getContent().open().openManagement());
+  const page = {
+    charset: 'utf-8',
+    code: 'test',
+    contentType: 'text/html',
+    pageModel: '1-2-column',
+    parentCode: 'homepage',
+    titles: {en: 'Test'},
+    ownerGroup: 'administrators'
+  };
 
-    const addTestContent = () => {
-      return cy.contentsController().then(controller => controller.addContent(testContent))
-               .then(response => cy.wrap(response.body.payload[0].id).as('contentToBeDeleted'));
-    };
+  const content = {
+    description: 'test',
+    mainGroup: 'administrators'
+  };
 
-    const addTestPage = () => {
-      cy.seoPagesController().then(controller => controller.addNewPage(page));
-      cy.contentTypesController().then(controller => controller.addContentType(contentTypeCode, contentTypeName));
-      cy.contentsController().then(controller => controller.addContent({...content, typeCode: contentTypeCode}))
-        .then((response) => {
-          const {body: {payload}} = response;
-          contentId               = payload[0].id;
-          cy.wrap(contentId).as('contentToBeDeleted');
-        });
-      cy.contentsController().then(controller => controller.updateStatus(contentId, 'published'));
-      cy.pageWidgetsController(page.code)
-        .then(controller => controller
-            .addWidget(0,
-                'search_form',
-                {
-                  ...pageWidget.config,
-                  contentId: contentId
-                }
-            )
-        );
-      cy.pagesController().then(controller => controller.setPageStatus(page.code, 'published'));
-    };
+  let contentId = {value: null};
 
-    const removeTestPage = () => {
-      cy.pageWidgetsController(page.code).then(controller => controller.deleteWidget(0));
-      cy.pagesController().then(controller => {
-        controller.setPageStatus(page.code, 'draft');
-        controller.deletePage(page.code);
+  const pageWidget = {
+    frameId: 4,
+    code: 'content_viewer',
+    config: {
+      ownerGroup: page.ownerGroup,
+      joinGroups: [],
+      contentDescription: content.description
+    }
+  };
+
+  const updatedDescription  = `${generateRandomId()}`;
+  const openContentMgmtPage = () => cy.get('@currentPage').then(page => page.getMenu().getContent().open().openManagement());
+
+  const addTestContent = () => {
+    return cy.contentsController().then(controller => controller.addContent(testContent))
+             .then(response => cy.wrap(response.body.payload[0].id).as('contentToBeDeleted'));
+  };
+
+  const addPublishedPage = () => {
+    cy.seoPagesController().then(controller => controller.addNewPage(page));
+    cy.contentTypesController().then(controller => controller.addContentType(contentTypeCode, contentTypeName));
+    cy.contentsController().then(controller => controller.addContent({...content, typeCode: contentTypeCode}))
+      .then((response) => {
+        const {body: {payload}} = response;
+        contentId               = payload[0].id;
+        cy.wrap(contentId).as('contentToBeDeleted');
       });
-      cy.contentsController().then(controller => {
-        controller.updateStatus(contentId, 'draft');
-        controller.deleteContent(contentId);
-      });
-      cy.contentTypesController().then(controller => controller.deleteContentType(contentTypeCode));
-    };
+    cy.contentsController().then(controller => controller.updateStatus(contentId, 'published'));
+    cy.pageWidgetsController(page.code)
+      .then(controller => controller
+          .addWidget(0,
+              'search_form',
+              {
+                ...pageWidget.config,
+                contentId: contentId
+              }
+          )
+      );
+    cy.pagesController().then(controller => controller.setPageStatus(page.code, 'published'));
+  };
 
-    const removePublishedContent = () => {
-      return cy.contentsController().then(controller => {
-        controller.updateStatus(content.id, 'draft');
-        controller.deleteContent(content.id);
-      });
-    };
+  const removePublishedPage = () => {
+    cy.pageWidgetsController(page.code).then(controller => controller.deleteWidget(0));
+    cy.pagesController().then(controller => {
+      controller.setPageStatus(page.code, 'draft');
+      controller.deletePage(page.code);
+    });
+    cy.contentsController().then(controller => {
+      controller.updateStatus(contentId, 'draft');
+      controller.deleteContent(contentId);
+    });
+    cy.contentTypesController().then(controller => controller.deleteContentType(contentTypeCode));
+  };
 
-    const addPublishedContent = () => {
-      cy.contentsController()
-        .then(controller => controller.addContent(testContent))
-        .then(response => content.id = response.body.payload[0].id);
-      cy.contentsController().then(controller => controller.updateStatus(content.id, 'published'))
-        .then(response => cy.wrap(response.body.payload[0].id).as('contentToBeDeleted'));
-    };
+  const removePublishedContent = () => {
+    return cy.contentsController().then(controller => {
+      controller.updateStatus(content.id, 'draft');
+      controller.deleteContent(content.id);
+    });
+  };
+
+  const addPublishedContent = () => {
+    cy.contentsController()
+      .then(controller => controller.addContent(testContent))
+      .then(response => content.id = response.body.payload[0].id);
+    cy.contentsController().then(controller => controller.updateStatus(content.id, 'published'))
+      .then(response => cy.wrap(response.body.payload[0].id).as('contentToBeDeleted'));
+  };
 
 });
