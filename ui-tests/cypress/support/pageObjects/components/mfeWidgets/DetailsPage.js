8754cdbe16e062286cdf1c1fc00b021acd3c390b
GianMartinoEntando <g.martino@entando.com>
2022-06-08 11:24:42 +0200

    openPage added in detailsPage.js

--- ui-tests/cypress/support/pageObjects/components/mfeWidgets/DetailsPage.js
+++ ui-tests/cypress/support/pageObjects/components/mfeWidgets/DetailsPage.js
@@ -1,3 +1,9 @@
 import AppContent from '../../app/AppContent';
 
-export default class DetailsPage extends AppContent {}
+export default class DetailsPage extends AppContent {
+  static openPage(button) {
+    cy.widgetsController().then(controller => controller.intercept({method: 'GET'}, 'detailsPageLoadingGET', '/content_viewer/info'));
+    cy.get(button).click();
+    cy.wait('@detailsPageLoadingGET');
+  }
+}
