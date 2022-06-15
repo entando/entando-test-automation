fcb80d7349991fe71d719a2a4bedfae9ae895c50
GianMartinoEntando <g.martino@entando.com>
2022-06-15 10:00:42 +0200

    clickAddButtonFromTableRowWithTitle() and addContent button added

--- ui-tests/cypress/support/pageObjects/pages/designer/widgetconfigs/ContentListWidgetConfigPage.js
+++ ui-tests/cypress/support/pageObjects/pages/designer/widgetconfigs/ContentListWidgetConfigPage.js
@@ -7,14 +7,25 @@ export default class ContentListWidgetConfigPage extends WidgetConfigPage {
     cy.contentTemplatesController().then(controller => controller.intercept({method: 'GET'}, 'contentModelsPageLoadingGET', '?*'));
     cy.contentsController().then(controller => controller.intercept({method: 'GET'}, 'contentsPageLoadingGET', '?*'));
     cy.contentTypesController().then(controller => controller.intercept({method: 'GET'}, 'contentTypesPageLoadingGET', '?*'));
-    cy.categoriesController().then(controller => controller.intercept({method: 'GET'}, 'homeCategoriesPageLoadingGET', '/home'));
+    //cy.categoriesController().then(controller => controller.intercept({method: 'GET'}, 'homeCategoriesPageLoadingGET', '/home'));
     cy.languagesController().then(controller => controller.intercept({method: 'GET'}, 'languagesPageLoadingGET', '?*'));
-    cy.pagesController().then(controller => controller.intercept({method: 'GET'}, 'searchPagesPageLoadingGET', '/search?*'));
+    //cy.pagesController().then(controller => controller.intercept({method: 'GET'}, 'searchPagesPageLoadingGET', '/search?*'));
     cy.pagesController().then(controller => controller.intercept({method: 'GET'}, 'pagePageLoadingGET', `/${code}?status=draft`));
-    cy.usersController().then(controller => controller.intercept({method: 'GET'}, 'usersPageLoadingGET', '?*'));
+    //cy.usersController().then(controller => controller.intercept({method: 'GET'}, 'usersPageLoadingGET', '?*'));
     cy.pagesController().then(controller => controller.intercept({method: 'GET'}, 'pageDraftWidgetsPageLoadingGET', `/${code}/widgets?status=draft`));
     cy.pagesController().then(controller => controller.intercept({method: 'GET'}, 'pagePublishedWidgetsPageLoadingGET', `/${code}/widgets?status=published`));
-    cy.wait(['@categoriesPageLoadingGET', '@contentModelsPageLoadingGET', '@contentsPageLoadingGET', '@contentTypesPageLoadingGET', '@homeCategoriesPageLoadingGET', '@languagesPageLoadingGET', '@searchPagesPageLoadingGET', '@pagePageLoadingGET', '@usersPageLoadingGET', '@pageDraftWidgetsPageLoadingGET', '@pagePublishedWidgetsPageLoadingGET']);
+    cy.wait(['@categoriesPageLoadingGET', '@contentModelsPageLoadingGET', '@contentsPageLoadingGET', '@contentTypesPageLoadingGET', /*'@homeCategoriesPageLoadingGET',*/ '@languagesPageLoadingGET', /*'@searchPagesPageLoadingGET',*/ '@pagePageLoadingGET', /*'@usersPageLoadingGET',*/ '@pageDraftWidgetsPageLoadingGET', '@pagePublishedWidgetsPageLoadingGET']);
+  }
+  //TODO find a way to remove arbitrary wait
+  static addContent(button){
+    cy.contentTypesController().then(controller => controller.intercept({method: 'GET'}, 'contentLoadingGET', `/?*`));
+    cy.get(button).click();
+    cy.wait('@contentLoadingGET');
+    cy.wait(500);
+  }
+  static settings(){
+    cy.contentTypesController().then(controller => controller.intercept({method: 'GET'}, 'contentLoadingGET', `/?*`));
+    cy.wait('@contentLoadingGET');
   }
 
   getContentListTableBody() {
@@ -28,8 +39,13 @@ export default class ContentListWidgetConfigPage extends WidgetConfigPage {
   getAddButtonFromTableRowWithTitle(title) {
     // FIXME - amend test id attributes for the buttons in appbuilder to avoid using `contains` method
     return this.getContentListTableRowWithTitle(title)
-               .find('button.btn.btn-default').contains(/^Add$/);
+               .find('button.btn.btn-default')//.contains(/^Add$/);
   }
+  clickAddButtonFromTableRowWithTitle(title) {
+    this.getAddButtonFromTableRowWithTitle(title).then(button => ContentListWidgetConfigPage.addContent(button));
+    return cy.get('@currentPage');
+  }
+
 
   getModelIdDropdownByIndex(idx) {
     return this.getMainContainer().find(`[name="contents[${idx}].modelId"]`);
