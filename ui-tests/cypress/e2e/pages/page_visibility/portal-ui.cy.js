3fdacd16f0b9f3d55199c5957306a6d8c33939ee
g.dimartino <g.dimartino@entando.com>
2022-07-15 17:42:53 +0200

    Added additional test cases for users without role and users without group

--- ui-tests/cypress/e2e/pages/page_visibility/portal-ui.cy.js
+++ ui-tests/cypress/e2e/pages/page_visibility/portal-ui.cy.js
@@ -113,6 +113,124 @@ describe('Page visibility in Portal UI', () => {
 
     });
 
+    describe(`User without role with ${groupPermission} group permission`, () => {
+
+      before(() => {
+        cy.kcClientCredentialsLogin();
+        cy.fixture('users/details/user').then(user =>
+            cy.usersController().then(controller =>
+                controller.addAuthorities(user.username, groupPermission, null)));
+      });
+
+      after(() => {
+        cy.kcClientCredentialsLogin();
+        cy.fixture('users/details/user').then(user =>
+            cy.usersController().then(controller =>
+                controller.deleteAuthorities(user.username)));
+      });
+
+      TEST_GROUPS.forEach(ownerGroup => {
+
+        describe(`Page with ${ownerGroup} owner group`, () => {
+
+          before(() => {
+            cy.kcClientCredentialsLogin();
+            cy.fixture('data/demoPage.json').then(demoPage => {
+              demoPage.ownerGroup = ownerGroup;
+              cy.seoPagesController().then(controller => controller.addNewPage(demoPage));
+              cy.pagesController().then(controller => controller.setPageStatus(demoPage.code, 'published'));
+            });
+          });
+
+          after(() =>
+              cy.fixture('data/demoPage.json').then(demoPage =>
+                  cy.pagesController().then(controller => {
+                    controller.setPageStatus(demoPage.code, 'draft');
+                    controller.deletePage(demoPage.code);
+                  })
+              ));
+
+          afterEach(() => cy.kcTokenLogout());
+
+          if (ownerGroup === groupPermission) {
+            it([Tag.SMOKE, 'ENG-3966'], `A user with "${groupPermission}" permission SHOULD be able to view a page with "${ownerGroup}" owner group and no join group`, () => checkPermission());
+          } else {
+            const permission = (ownerGroup === 'free' || groupPermission === 'administrators');
+            const visibility = permission ? ' ' : ' NOT ';
+
+            it([Tag.SANITY, 'ENG-3966'], `A user with "${groupPermission}" permission SHOULD${visibility}be able to view a page with "${ownerGroup}" owner group and no join group`, () => checkPermission(permission));
+          }
+
+          TEST_GROUPS.filter(group => (group !== ownerGroup)).forEach(joinGroup => {
+            if (ownerGroup === groupPermission || joinGroup === groupPermission || (joinGroup === 'free' && (ownerGroup === 'group1' || ownerGroup === 'group2'))) {
+              it([Tag.FEATURE, 'ENG-3966'], `A user with "${groupPermission}" permission SHOULD be able to view a page with "${ownerGroup}" owner group and "${joinGroup}" join group`, () => {
+                cy.fixture('data/demoPage.json').then(demoPage => {
+                  cy.kcClientCredentialsLogin();
+                  demoPage.ownerGroup = ownerGroup;
+                  cy.seoPagesController().then(controller => controller.setPageJoinGroups(demoPage, [joinGroup]));
+                  cy.pagesController().then(controller => controller.setPageStatus(demoPage.code, 'published'));
+                  checkPermission();
+                });
+              });
+            }
+          });
+
+        });
+
+      });
+
+    });
+
+  });
+
+  describe(`User without group permission`, () => {
+
+    before(() => {
+      cy.kcClientCredentialsLogin();
+      cy.fixture('users/details/user').then(user =>
+          cy.usersController().then(controller =>
+              controller.addAuthorities(user.username, null, 'admin')));
+    });
+
+    after(() => {
+      cy.kcClientCredentialsLogin();
+      cy.fixture('users/details/user').then(user =>
+          cy.usersController().then(controller =>
+              controller.deleteAuthorities(user.username)));
+    });
+
+    TEST_GROUPS.forEach(ownerGroup => {
+
+      describe(`Page with ${ownerGroup} owner group`, () => {
+
+        before(() => {
+          cy.kcClientCredentialsLogin();
+          cy.fixture('data/demoPage.json').then(demoPage => {
+            demoPage.ownerGroup = ownerGroup;
+            cy.seoPagesController().then(controller => controller.addNewPage(demoPage));
+            cy.pagesController().then(controller => controller.setPageStatus(demoPage.code, 'published'));
+          });
+        });
+
+        after(() =>
+            cy.fixture('data/demoPage.json').then(demoPage =>
+                cy.pagesController().then(controller => {
+                  controller.setPageStatus(demoPage.code, 'draft');
+                  controller.deletePage(demoPage.code);
+                })
+            ));
+
+        afterEach(() => cy.kcTokenLogout());
+
+        const permission = ownerGroup === 'free';
+        const visibility = permission ? ' ' : ' NOT ';
+
+        it([Tag.SANITY, 'ENG-3966'], `A user without group permission SHOULD${visibility}be able to view a page with "${ownerGroup}" owner group and no join group`, () => checkPermission(permission));
+
+      });
+
+    });
+
   });
 
 });
