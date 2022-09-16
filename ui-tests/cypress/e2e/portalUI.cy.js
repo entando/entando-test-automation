38c7f44517c6c0f239a608639b66ab00c13679e2
g.dimartino <g.dimartino@entando.com>
2022-09-16 15:35:42 +0200

    Added test for ENG-4181

---
+++ ui-tests/cypress/e2e/portalUI.cy.js
@@ -0,0 +1,21 @@
+describe('Portal UI', () => {
+
+  it('ENG-4181', 'Non-existent URL MUST return 404 status code and Page not found page', () => {
+    const baseURL = Cypress.config('portalUIPath');
+    const pageURL = '/cypress.page';
+
+    cy.request({
+      url: baseURL + pageURL,
+      followRedirect: false,
+      failOnStatusCode: false
+    }).then((resp) => {
+      expect(resp.status).to.eq(404);
+      expect(resp.redirectedToUrl).to.eq(undefined);
+    });
+    cy.visit('/cypress.page', {portalUI: true, failOnStatusCode: false});
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq(`${baseURL}/notfound.page`);
+    });
+  });
+
+});
