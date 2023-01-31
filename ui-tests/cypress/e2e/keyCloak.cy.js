53598cdc4645917a20e8d7921e81196070724a32
riccardo-fadda <r.fadda@entando.com>
2023-01-31 16:27:19 +0100

    ENG-4525 Added test to check the first login process

--- ui-tests/cypress/e2e/keyCloak.cy.js
+++ ui-tests/cypress/e2e/keyCloak.cy.js
@@ -6,8 +6,19 @@ describe('Keycloak', () => {
   const authBaseUrl = Cypress.env('auth_base_url').split('/');
   const origin      = `${authBaseUrl[0]}//${authBaseUrl[2]}`;
 
-  const realm    = Cypress.env('auth_realm');
-  const pathName = `/${authBaseUrl[3]}/realms/${realm}/protocol/openid-connect/auth`;
+  const realm              = Cypress.env('auth_realm');
+  const pathName           = `/${authBaseUrl[3]}/realms/${realm}/protocol/openid-connect/auth`;
+  const passwordUpdatePath = `/${authBaseUrl[3]}/realms/${realm}/login-actions/required-action`;
+
+  beforeEach(() => {
+    cy.wrap(null).as('userToBeDeleted');
+  });
+
+  afterEach(() => {
+    cy.get('@userToBeDeleted').then(user => {
+      if (user) cy.usersController().then(controller => controller.deleteUser(user));
+    });
+  });
 
   const checkLocation = (origin, pathname) => {
     cy.location().should((location) => {
@@ -46,6 +57,37 @@ describe('Keycloak', () => {
         page.getNavbar().openUserMenu().logout();
       });
     checkLocation(origin, pathName);
+  });
+
+  it([Tag.FEATURE, 'ENG-4525'], 'A new user should be required to update the password only on first login', () => {
+    cy.kcClientCredentialsLogin();
+    cy.fixture('users/details/user').then(user => {
+      cy.usersController().then(controller => {
+        controller.addUser(user).then(() => cy.wrap(user.username).as('userToBeDeleted'));
+        controller.addAuthorities(user.username, 'administrators', 'admin');
+      });
+    });
+
+    cy.visit('/');
+    checkLocation(origin, pathName);
+
+    cy.fixture(`users/login/user`)
+      .then(userData => {
+        cy.wrap(new LoginPage())
+          .then(page => {
+            page.login(userData);
+            checkLocation(origin, passwordUpdatePath);
+            page.confirmPassword(userData);
+            checkLocation(Cypress.config('baseUrl'), Cypress.config('basePath') + '/dashboard');
+
+            cy.kcLogout();
+
+            cy.visit('/');
+            checkLocation(origin, pathName);
+            page.login(userData);
+            checkLocation(Cypress.config('baseUrl'), Cypress.config('basePath') + '/dashboard');
+          });
+      });
   });
 
 });
--- ui-tests/cypress/support/pageObjects/keycloak/LoginPage.js
+++ ui-tests/cypress/support/pageObjects/keycloak/LoginPage.js
@@ -7,6 +7,7 @@ export default class LoginPage extends WebElement {
   constructor(parent) {
     super(parent);
     this.loginForm = new LoginForm(this);
+    this.passwordUpdateForm = new PasswordUpdateForm(this);
   }
 
   get() {
@@ -19,9 +20,18 @@ export default class LoginPage extends WebElement {
     return this.loginForm;
   }
 
+  getPasswordUpdateForm() {
+    return this.passwordUpdateForm;
+  }
+
   login(userData) {
     const loginForm = this.getLoginForm();
     loginForm.login(userData);
+  }
+
+  confirmPassword(userData) {
+    const passwordUpdateForm = this.getPasswordUpdateForm();
+    passwordUpdateForm.confirmPassword(userData.password);
   }
 
 }
@@ -30,9 +40,9 @@ class LoginForm extends WebElement {
 
   formWrapper  = `${htmlElements.div}.LoginPage__formWrapper`;
   form         = `${htmlElements.form}.LoginPage__form`;
-  username     = '#username';
-  password     = '#password';
-  submitButton = '.LoginPage__button[type=submit]';
+  username     = `${htmlElements.input}#username`;
+  password     = `${htmlElements.input}#password`;
+  submitButton = `${htmlElements.button}.LoginPage__button[type=submit]`;
 
   get() {
     return this.parent.get()
@@ -71,6 +81,55 @@ class LoginForm extends WebElement {
     this.typeUsername(userData.username);
     this.typePassword(userData.password);
     this.submitForm();
+  }
+
+}
+
+class PasswordUpdateForm extends WebElement {
+
+  formWrapper        = `${htmlElements.div}.LoginPage__formWrapper`;
+  form               = `${htmlElements.form}#kc-passwd-update-form`;
+  newPassword        = `${htmlElements.input}#password-new`;
+  newPasswordConfirm = `${htmlElements.input}#password-confirm`;
+  newPasswordSubmit  = `${htmlElements.input}.btn[type=submit]`;
+
+  get() {
+    return this.parent.get()
+               .children(this.formWrapper)
+               .children(this.form);
+  }
+
+  getNewPassword() {
+    return this.get()
+               .find(this.newPassword);
+  }
+
+  getNewPasswordConfirm() {
+    return this.get()
+               .find(this.newPasswordConfirm);
+  }
+
+  getNewPasswordSubmit() {
+    return this.get()
+               .find(this.newPasswordSubmit);
+  }
+
+  typeNewPassword(input) {
+    this.getNewPassword().type(input);
+  }
+
+  typeNewPasswordConfirm(input) {
+    this.getNewPasswordConfirm().type(input);
+  }
+
+  submitNewPassword() {
+    this.getNewPasswordSubmit().click();
+  }
+
+  confirmPassword(password) {
+    this.typeNewPassword(password);
+    this.typeNewPasswordConfirm(password);
+    this.submitNewPassword();
   }
 
 }
