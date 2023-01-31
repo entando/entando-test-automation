166c68fefb408eca50b48d573da48594ef93fac4
riccardo-fadda <r.fadda@entando.com>
2023-01-31 16:05:40 +0100

    ENG-4525 refined keyCloak spec file to decrease code repetition

--- ui-tests/cypress/e2e/keyCloak.cy.js
+++ ui-tests/cypress/e2e/keyCloak.cy.js
@@ -1,7 +1,7 @@
 import LoginPage from '../support/pageObjects/keycloak/LoginPage.js';
 import HomePage  from '../support/pageObjects/HomePage';
 
-describe([Tag.SMOKE], 'Keycloack', () => {
+describe('Keycloak', () => {
 
   const authBaseUrl = Cypress.env('auth_base_url').split('/');
   const origin      = `${authBaseUrl[0]}//${authBaseUrl[2]}`;
@@ -9,51 +9,43 @@ describe([Tag.SMOKE], 'Keycloack', () => {
   const realm    = Cypress.env('auth_realm');
   const pathName = `/${authBaseUrl[3]}/realms/${realm}/protocol/openid-connect/auth`;
 
-  it('Login via API client_credentials', () => {
+  const checkLocation = (origin, pathname) => {
+    cy.location().should((location) => {
+      expect(location.origin).to.eq(origin);
+      expect(location.pathname).to.eq(pathname);
+    });
+  };
+
+  it([Tag.SMOKE], 'Login via API client_credentials', () => {
     cy.kcClientCredentialsLogin();
   });
 
-  it('Login/Logout via API authorization_code', () => {
+  it([Tag.SMOKE], 'Login/Logout via API authorization_code', () => {
     cy.kcAuthorizationCodeLoginAndOpenDashboard('login/admin');
-    cy.location().should((location) => {
-      expect(location.origin).to.eq(Cypress.config('baseUrl'));
-      expect(location.pathname).to.eq(Cypress.config('basePath') + '/dashboard');
-    });
+    checkLocation(Cypress.config('baseUrl'), Cypress.config('basePath') + '/dashboard');
 
     cy.kcTokenLogout();
 
     cy.visit('/');
-    cy.location().should((location) => {
-      expect(location.origin).to.eq(origin);
-      expect(location.pathname).to.eq(pathName);
-    });
+    checkLocation(origin, pathName);
   });
 
-  it('Login/Logout via UI', () => {
+  it([Tag.SMOKE], 'Login/Logout via UI', () => {
     cy.visit('/');
-    cy.location().should((location) => {
-      expect(location.origin).to.eq(origin);
-      expect(location.pathname).to.eq(pathName);
-    });
+    checkLocation(origin, pathName);
 
     cy.fixture(`users/login/admin`)
       .then(userData =>
-          cy.wrap(new LoginPage())
-            .then(page => page.login(userData)));
-    cy.location().should((location) => {
-      expect(location.origin).to.eq(Cypress.config('baseUrl'));
-      expect(location.pathname).to.eq(Cypress.config('basePath') + '/dashboard');
-    });
+        cy.wrap(new LoginPage())
+          .then(page => page.login(userData)));
+    checkLocation(Cypress.config('baseUrl'), Cypress.config('basePath') + '/dashboard');
 
     cy.wrap(new HomePage())
       .then(page => {
         HomePage.openPage();
         page.getNavbar().openUserMenu().logout();
       });
-    cy.location().should((location) => {
-      expect(location.origin).to.eq(origin);
-      expect(location.pathname).to.eq(pathName);
-    });
+    checkLocation(origin, pathName);
   });
 
 });
